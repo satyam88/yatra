5d6381b030a5c62c85599448025ea2f058babf3f
Satyam Pandey <satyam@gmail.com>
2022-11-13 09:44:42 +0530

    Adding Dockerfile and visa.java

--- src/main/java/com/example/yatra/visa.java
+++ src/main/java/com/example/yatra/visa.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class visa {
     @GetMapping("/myvisa")
     public String getData(){
-        return "Please book visa";
+        return "Please book visa, book your cab";
     }
 
 }
